5292412fc957244406b833a9f6a387f4bcaf796a
Darren Kessner <Darren.Kessner@marlborough.org>
2021-08-18 14:04:34 -0700

    testing mouse UI for unit circle

--- hello/unit_circle.js
+++ hello/unit_circle.js
@@ -9,19 +9,21 @@ const s = function(sketch) {
  let radius;
 
  sketch.setup = function () {
-     let canvas = sketch.createCanvas(600, 400);
+     let canvas = sketch.createCanvas(600, 200);
      //canvas.parent("sketch");
-     axes_center_1 = new p5.Vector(sketch.width / 4, sketch.height / 2);
-     axes_center_2 = new p5.Vector(1.1 * sketch.width / 2, sketch.height / 2);
-     radius = .9 * sketch.width / 4;
+     axes_center_1 = new p5.Vector(sketch.width * .12, sketch.height / 2);
+     axes_center_2 = new p5.Vector(sketch.width * .3, sketch.height / 2);
+     radius = .9 * axes_center_1.x;
  }
 
  sketch.drawAxes = function() {
      sketch.strokeWeight(1);
-     sketch.stroke(0, 255, 0);
+     sketch.stroke(128);
+
      sketch.line(axes_center_1.x - radius, axes_center_1.y, axes_center_1.x + radius, axes_center_1.y);
      sketch.line(axes_center_1.x, axes_center_1.y - radius, axes_center_1.x, axes_center_1.y + radius);
-     sketch.line(axes_center_2.x - radius * .1, axes_center_2.y, axes_center_2.x + radius * 1.9, axes_center_2.y);
+
+     sketch.line(axes_center_2.x - radius * .1, axes_center_2.y, axes_center_2.x + radius * 2 * sketch.PI, axes_center_2.y);
      sketch.line(axes_center_2.x, axes_center_2.y - radius, axes_center_2.x, axes_center_2.y + radius);
  }
 
@@ -35,7 +37,7 @@ const s = function(sketch) {
  sketch.X = function(angle) {
      angle = angle % (2 * sketch.PI);
      if (angle < 0) angle += 2 * sketch.PI;
-     let x = axes_center_2.x + angle * radius * 1.9 / 2 / sketch.PI;
+     let x = axes_center_2.x + angle * radius;
      return x;
  }
 
@@ -45,6 +47,7 @@ const s = function(sketch) {
 
  sketch.drawSin = function() {
      sketch.strokeWeight(1);
+     sketch.stroke(200);
      sketch.beginShape();
      for (let t = 0; t < 2 * sketch.PI; t += .01) {
          sketch.vertex(this.X(t), this.Y(t));
@@ -53,20 +56,29 @@ const s = function(sketch) {
  }
 
  sketch.drawClockHand = function() {
-     sketch.strokeWeight(3);
-     sketch.stroke(255);
+     sketch.strokeWeight(5);
+     sketch.stroke(255, 0, 0);
      let x = axes_center_1.x + radius * sketch.cos(theta);
      let y = axes_center_1.y - radius * sketch.sin(theta);
      sketch.line(axes_center_1.x, axes_center_1.y, x, y);
+
+     sketch.stroke(200);
      sketch.line(axes_center_1.x, axes_center_1.y, axes_center_1.x, y);
  }
 
  sketch.drawVerticalBar = function() {
-     sketch.strokeWeight(3);
-     sketch.stroke(255);
-     let x = this.X(theta);
+     sketch.strokeWeight(5);
+     sketch.stroke(200);
+     let x = sketch.X(theta);
      let y = sketch.Y(theta);
      sketch.line(x, axes_center_2.y, x, y);
+ }
+
+ sketch.drawTheta = function() {
+     sketch.strokeWeight(4);
+     sketch.stroke(0, 0, 255);
+     sketch.arc(axes_center_1.x, axes_center_1.y, radius*2, radius*2, -theta, 0);
+     sketch.line(axes_center_2.x, axes_center_2.y, sketch.X(theta), axes_center_2.y);
  }
 
  sketch.draw = function() {
@@ -76,7 +88,24 @@ const s = function(sketch) {
      sketch.drawClockHand();
      sketch.drawSin();
      sketch.drawVerticalBar();
+     sketch.drawTheta();
      theta += dtheta;
+ }
+
+ sketch.mouseDragged = function() {
+     if (sketch.mouseX < axes_center_2.x)
+     {
+        let p1 = sketch.createVector(sketch.mouseX, sketch.mouseY);
+        p1.sub(axes_center_1);
+        let p2 = sketch.createVector(sketch.pmouseX, sketch.pmouseY);
+        p2.sub(axes_center_1);
+        let dtheta = p1.angleBetween(p2);
+        theta += dtheta;
+     }
+     else
+     {
+        theta = (sketch.mouseX - axes_center_2.x) / radius;
+     }
  }
 
  sketch.keyPressed = function() {
